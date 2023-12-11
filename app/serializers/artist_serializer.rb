b8f3734ace2319efa29a15b071fda80f63003858
Pascal Odudigbo <odudigbopascal@gmail.com>
2023-12-11 16:43:55 +0000

    modifying the serializers to display relevant data

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,3 +1,9 @@
 class ArtistSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :stage_name, :email, :verified, :password_digest
+  has_one :artists_profile
+  has_one :artists_social
+  has_many :projects do
+    object.projects.order(created_at: :desc)
+  end 
+  has_many :project_videos, through: :projects
 end
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :title, :project_url, :cover_art, :cover_art_public_id
   has_one :artist
+  has_many :project_videos
 end
